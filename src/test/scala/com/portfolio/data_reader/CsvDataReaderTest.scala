d9f9fa05e12f3132ea5e13c9d6ef44f85a6d7893
barakm <barakm@wix.com>
2020-06-13 22:57:31 +0300

    Created FileReader test

---
+++ src/test/scala/com/portfolio/data_reader/CsvDataReaderTest.scala
@@ -0,0 +1,20 @@
+package com.portfolio.data_reader
+
+import org.joda.time.DateTime
+import org.specs2.mutable.Specification
+
+class CsvDataReaderTest extends Specification {
+  private val reader = new CsvDataReader("src/test/resources/market-portfolio")
+  "CsvDataReader" should {
+    "read file with one line" in {
+      reader.readFiles() must containTheSameElementsAs(
+        Seq(
+          IndexRawData("csv-with-one-line.csv", Seq(RawLine(DateTime.parse("2015-06-01"), 186.277267))),
+          IndexRawData("csv-with-two-lines.csv",
+            Seq(
+              RawLine(DateTime.parse("2015-06-01"), 186.277267),
+              RawLine(DateTime.parse("2015-07-01"), 191.411682)))
+        ))
+    }
+  }
+}
