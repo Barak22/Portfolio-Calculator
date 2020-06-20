a151c5e9a58e19b219aa8e47888b3e7ded0e7420
barakm <barakm@wix.com>
2020-06-21 00:47:01 +0300

    added more logic to the service class

--- src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
+++ src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
@@ -1,20 +1,47 @@
 package com.portfolio.service
 
+import com.portfolio.cov.CovCalculator
 import com.portfolio.data_reader.DataReader
-import com.portfolio.domain.{ IndexRawData, IndexReturnData, ReturnData }
+import com.portfolio.domain.{ CovData, IndexRawData, IndexReturnData, ReturnData }
 import com.portfolio.returns.ReturnsCalculator
 
 class PortfolioCalculatorService(dataReader: DataReader,
-                                 returnsCalculator: ReturnsCalculator) {
+                                 returnsCalculator: ReturnsCalculator,
+                                 covCalculator: CovCalculator) {
   def calculateMarketPortfolio() = {
     val indexesRawData: Seq[IndexRawData] = dataReader.readFiles()
-    val indexesReturns = indexesRawData.map {
+    val indexesReturns: Seq[IndexReturnData] = indexesRawData.map {
       indexRawData =>
         val indexReturns: Seq[ReturnData] = returnsCalculator.calculateReturns(indexRawData.stockData)
         IndexReturnData(indexRawData.stockFileName, indexReturns)
-
-      // Calculate Cov matrix.
-      // find the min variance vector for a desired E(r).
     }
+
+    val indexesEr: Map[String, Double] = indexesReturns
+      .map(i =>
+        i.stockFileName -> PortfolioCalculatorService.calAverageReturn(i.stockData.map(_.r))).toMap
+
+    val covData: Seq[CovData] =
+      for {
+        s1 <- indexesReturns
+        s2 <- indexesReturns
+      } yield {
+        val r1 = s1.stockData.map(_.r)
+        val r2 = s2.stockData.map(_.r)
+        val s1Er = indexesEr(s1.stockFileName)
+        val s2Er = indexesEr(s2.stockFileName)
+
+        val cov = covCalculator.clacCov(r1, r2, s1Er, s2Er)
+        CovData(s1.stockFileName, s2.stockFileName, cov)
+      }
+
+    // find the min variance vector for a desired E(r).
+  }
+
+
+}
+
+object PortfolioCalculatorService {
+  def calAverageReturn(returns: Seq[Double]) = {
+    returns.foldLeft(0d)((acc, r) => acc + r) / returns.size
   }
 }
--- src/test/scala/com/portfolio/service/PortfolioCalculatorServiceTest.scala
+++ src/test/scala/com/portfolio/service/PortfolioCalculatorServiceTest.scala
@@ -1,5 +1,14 @@
 package com.portfolio.service
 
-class PortfolioCalculatorServiceTest {
+import org.specs2.mutable.Specification
+
+class PortfolioCalculatorServiceTest extends Specification {
+  "PortfolioCalculatorService" should {
+    "calc average" in {
+      PortfolioCalculatorService.calAverageReturn(Seq(1)) must beEqualTo(1)
+      PortfolioCalculatorService.calAverageReturn(Seq(1,2)) must beEqualTo(1.5)
+      PortfolioCalculatorService.calAverageReturn(Seq(1,2,3,4,5)) must beEqualTo(3)
+    }
+  }
 
 }
