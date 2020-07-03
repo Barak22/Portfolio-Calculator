dea99f0911d57414f22d27136eee06870ffa0901
barakm <barakm@wix.com>
2020-07-03 13:21:40 +0300

    Refactored the classes a bit

--- src/test/scala/com/portfolio/variance/DefaultVarianceCalculatorTest.scala
+++ src/test/scala/com/portfolio/variance/DefaultVarianceCalculatorTest.scala
@@ -12,30 +12,30 @@ class DefaultVarianceCalculatorTest extends Specification {
 
     "calcVariance" >> {
       "calculate the variance for 1 stock 1 vector" in new Context {
-        varianceCalculator.calcVariance(Seq(VectorWeights(Seq(StockWeight("stock1", 0.03)))), Seq(CovData("stock1", "stock1", 0.00001))) must
-          containTheSameElementsAs(Seq(VectorVariance(Seq(StockWeight("stock1", 0.03)), 0.000000009)))
+        varianceCalculator.calcVariance(Seq(VectorWeights(Seq(StockWeight(stockName1, 0.03)))), Seq(CovData(stockName1, stockName1, 0.00001))) must
+          containTheSameElementsAs(Seq(VectorVariance(Seq(StockWeight(stockName1, 0.03)), 0.000000009)))
       }
 
       "calculate the variance for 2 stocks 1 vector" in new Context {
         varianceCalculator.calcVariance(
-          Seq(VectorWeights(Seq(StockWeight("stock1", 0.02), StockWeight("stock2", 0.01)))),
-          Seq(CovData("stock1", "stock1", 0.00001),
-            CovData("stock1", "stock2", 0.00004),
-            CovData("stock2", "stock1", 0.00004),
-            CovData("stock2", "stock2", 0.00009))) must
-          containTheSameElementsAs(Seq(VectorVariance(Seq(StockWeight("stock1", 0.02), StockWeight("stock2", 0.01)), 0.000000029)))
+          Seq(VectorWeights(Seq(StockWeight(stockName1, 0.02), StockWeight(stockName2, 0.01)))),
+          Seq(CovData(stockName1, stockName1, 0.00001),
+            CovData(stockName1, stockName2, 0.00004),
+            CovData(stockName2, stockName1, 0.00004),
+            CovData(stockName2, stockName2, 0.00009))) must
+          containTheSameElementsAs(Seq(VectorVariance(Seq(StockWeight(stockName1, 0.02), StockWeight(stockName2, 0.01)), 0.000000029)))
       }
 
       "calculate the variance for 2 stocks and 2 vectors" in new Context {
         varianceCalculator.calcVariance(
-          Seq(VectorWeights(Seq(StockWeight("stock1", 0.03), StockWeight("stock2", 0))), VectorWeights(Seq(StockWeight("stock1", 0.02), StockWeight("stock2", 0.01)))),
-          Seq(CovData("stock1", "stock1", 0.00001),
-            CovData("stock1", "stock2", 0.00004),
-            CovData("stock2", "stock1", 0.00004),
-            CovData("stock2", "stock2", 0.00009))) must
+          Seq(VectorWeights(Seq(StockWeight(stockName1, 0.03), StockWeight(stockName2, 0))), VectorWeights(Seq(StockWeight(stockName1, 0.02), StockWeight(stockName2, 0.01)))),
+          Seq(CovData(stockName1, stockName1, 0.00001),
+            CovData(stockName1, stockName2, 0.00004),
+            CovData(stockName2, stockName1, 0.00004),
+            CovData(stockName2, stockName2, 0.00009))) must
           containTheSameElementsAs(
-            Seq(VectorVariance(Seq(StockWeight("stock1", 0.03), StockWeight("stock2", 0)), 0.000000009),
-              VectorVariance(Seq(StockWeight("stock1", 0.02), StockWeight("stock2", 0.01)), 0.000000029)))
+            Seq(VectorVariance(Seq(StockWeight(stockName1, 0.03), StockWeight(stockName2, 0)), 0.000000009),
+              VectorVariance(Seq(StockWeight(stockName1, 0.02), StockWeight(stockName2, 0.01)), 0.000000029)))
       }
 
     }
@@ -44,6 +44,8 @@ class DefaultVarianceCalculatorTest extends Specification {
 
 
   trait Context extends Scope {
+    val stockName1 = "stock1"
+    val stockName2 = "stock2"
   }
 
 }
