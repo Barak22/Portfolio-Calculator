b326c58b14d93338357769dfc1def5efbea49d0f
barakm <barakm@wix.com>
2020-07-04 13:38:33 +0300

    Renamed variables and added println for debuging

--- src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
+++ src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
@@ -3,7 +3,7 @@ package com.portfolio.service
 import com.portfolio.cov.CovCalculator
 import com.portfolio.data_reader.DataReader
 import com.portfolio.domain.{ CovData, IndexRawData, ReturnData, StockReturnData }
-import com.portfolio.returns.ReturnsCalculator
+import com.portfolio.returns.{ PortfolioReturnCalculator, ReturnsCalculator }
 import com.portfolio.variance.VarianceCalculator
 import com.portfolio.vector.VectorCreator
 
@@ -11,17 +11,19 @@ class PortfolioCalculatorService(dataReader: DataReader,
                                  returnsCalculator: ReturnsCalculator,
                                  covCalculator: CovCalculator,
                                  varianceCalculator: VarianceCalculator,
+                                 portfolioReturnCalculator: PortfolioReturnCalculator,
                                  desiredReturn: 10) {
   def calculateMarketPortfolio() = {
     val indexesRawData: Seq[IndexRawData] = dataReader.readFiles()
     val stocksNames = indexesRawData.map(_.stockFileName)
     val indexesReturns: Seq[StockReturnData] = indexesRawData.map {
       indexRawData =>
+//        println(s"indexesRawData = ${indexesRawData}")
         val indexReturns: Seq[ReturnData] = returnsCalculator.calculateReturns(indexRawData.stockData)
         StockReturnData(indexRawData.stockFileName, indexReturns)
     }
 
-    val indexesEr: Map[String, Double] = indexesReturns
+    val stocksEr: Map[String, Double] = indexesReturns
       .map(i =>
         i.stockFileName -> PortfolioCalculatorService.calcAverageReturn(i.stockData.map(_.r))).toMap
 
@@ -32,15 +34,21 @@ class PortfolioCalculatorService(dataReader: DataReader,
       } yield {
         val s1Returns = s1.stockData.map(_.r)
         val s2Returns = s2.stockData.map(_.r)
-        val s1Er = indexesEr(s1.stockFileName)
-        val s2Er = indexesEr(s2.stockFileName)
+        val s1Er = stocksEr(s1.stockFileName)
+        val s2Er = stocksEr(s2.stockFileName)
 
         val cov = covCalculator.clacCov(s1Returns, s2Returns, s1Er, s2Er)
         CovData(s1.stockFileName, s2.stockFileName, cov)
       }
 
     val vectors = VectorCreator.createVectors(stocksNames)
-    varianceCalculator.calcVariance(vectors, covData)
+    val vectorsForDesiredEr = portfolioReturnCalculator.calcReturn(stocksEr, vectors)
+//      .map()
+//        .max
+        .filter(v => v.Er > ((desiredReturn - 2).toDouble / 100) && v.Er < (desiredReturn + 2).toDouble / 100)
+    println(s"stocksEr = ${stocksEr}")
+    println(s"vectorsForDesiredEr = ${vectorsForDesiredEr}")
+//    varianceCalculator.calcVariance(vectorsForDesiredEr, covData)
 
     // find the min variance vector for a desired E(r).
   }
@@ -50,6 +58,6 @@ class PortfolioCalculatorService(dataReader: DataReader,
 
 object PortfolioCalculatorService {
   def calcAverageReturn(returns: Seq[Double]) = {
-    returns.foldLeft(0d)((acc, r) => acc + r) / returns.size
+    returns.sum / returns.size
   }
 }
