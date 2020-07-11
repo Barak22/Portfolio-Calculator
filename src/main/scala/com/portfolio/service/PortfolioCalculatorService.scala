cc6dc894a52e03a46e424d2638ec9347a0459b87
barakm <barakm@wix.com>
2020-07-11 23:23:47 +0300

    Refactored the method a bit and added measures

--- src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
+++ src/main/scala/com/portfolio/service/PortfolioCalculatorService.scala
@@ -2,7 +2,7 @@ package com.portfolio.service
 
 import com.portfolio.cov.CovCalculator
 import com.portfolio.data_reader.DataReader
-import com.portfolio.domain.{ CovData, IndexRawData, ReturnData, StockReturnData }
+import com.portfolio.domain._
 import com.portfolio.measure.DurationMeasurer
 import com.portfolio.returns.{ PortfolioReturnCalculator, ReturnsCalculator }
 import com.portfolio.stdev.STDEVCalculator
@@ -17,56 +17,71 @@ class PortfolioCalculatorService(dataReader: DataReader,
                                  desiredReturn: Int,
                                  measurer: DurationMeasurer) {
 
-  // TODO: Need to refactor this method
   def calculateMarketPortfolio(): Unit = {
     val indexesRawData: Seq[IndexRawData] = dataReader.readFiles()
     val stocksNames = indexesRawData.map(_.stockFileName)
-    val indexesReturns: Seq[StockReturnData] = indexesRawData.map {
-      indexRawData =>
-        val indexReturns: Seq[ReturnData] = returnsCalculator.calculateReturns(indexRawData.stockData)
-        StockReturnData(indexRawData.stockFileName, indexReturns)
-    }
+    val indexesReturns: Seq[StockReturnData] = calculateStockReturns(indexesRawData)
 
+    val monthlyStocksEr: Map[String, Double] = calculateTheMonthlyAverageReturnPerStock(indexesReturns)
+    val yearlyStocksEr: Map[String, Double] = calculateTheAnnualAverageReturnPerStock(indexesReturns)
 
-    val monthlyStocksEr: Map[String, Double] = indexesReturns
-      .map(i =>
-        i.stockFileName -> PortfolioCalculatorService.calcAverageReturn(i.stockData.map(_.r))).toMap
+    val covData: Seq[CovData] = calculateStocksCovariance(indexesReturns, monthlyStocksEr)
 
-    val yearlyStocksEr: Map[String, Double] = indexesReturns
-      .map(i =>
-        i.stockFileName -> PortfolioCalculatorService.calcYearlyAverageReturn(i.stockData.map(_.r))).toMap
-
-    val covData: Seq[CovData] =
-      for {
-        s1 <- indexesReturns
-        s2 <- indexesReturns
-      } yield {
-        val s1Returns = s1.stockData.map(_.r)
-        val s2Returns = s2.stockData.map(_.r)
-        val s1Er = monthlyStocksEr(s1.stockFileName)
-        val s2Er = monthlyStocksEr(s2.stockFileName)
-
-        val cov = covCalculator.clacCov(s1Returns, s2Returns, s1Er, s2Er)
-        CovData(s1.stockFileName, s2.stockFileName, cov)
-      }
-
-    println(s"covData = $covData")
+    //    println(s"covData = $covData")
     val vectors = measurer.measure("VectorCreator.createVectors", VectorCreator.createVectors(stocksNames))
-    val vectorsForDesiredEr = portfolioReturnCalculator.calcReturn(yearlyStocksEr, vectors)
-      .filter(v => v.Er > ((desiredReturn - 2).toDouble / 100) && v.Er < (desiredReturn + 2).toDouble / 100)
-    println(s"monthlyStocksEr = $monthlyStocksEr")
-    println(s"yearlyStocksEr = $yearlyStocksEr")
-    println(s"vectorsForDesiredEr = $vectorsForDesiredEr")
-    val vectorsWithVariance = varianceCalculator.calcVariance(vectorsForDesiredEr, covData)
-    val vectorsWithStandardDeviation = STDEVCalculator.calculateStdev(vectorsWithVariance)
+    val vectorsForDesiredEr = measurer.measure("filterVectorsWhichComplyDesiredReturn", filterVectorsWhichComplyDesiredReturn(yearlyStocksEr, vectors))
+    //    println(s"monthlyStocksEr = $monthlyStocksEr")
+    //    println(s"yearlyStocksEr = $yearlyStocksEr")
+    //    println(s"vectorsForDesiredEr = $vectorsForDesiredEr")
+    val vectorsWithVariance = measurer.measure("varianceCalculator.calcVariance", varianceCalculator.calcVariance(vectorsForDesiredEr, covData))
+    val vectorsWithStandardDeviation = measurer.measure("STDEVCalculator.calculateStdev", STDEVCalculator.calculateStdev(vectorsWithVariance))
 
     vectorsWithStandardDeviation
       .sortBy(vectorStdev => (vectorStdev.stdev, vectorStdev.Er))
 
-    println(s"vectorsWithVariance = ${vectorsWithVariance}")
+    //    println(s"vectorsWithVariance = $vectorsWithVariance")
   }
 
 
+  private def filterVectorsWhichComplyDesiredReturn(yearlyStocksEr: Map[String, Double], vectors: Seq[VectorWeights]) = {
+    portfolioReturnCalculator.calcReturn(yearlyStocksEr, vectors)
+      .filter(v => v.Er > ((desiredReturn - 2).toDouble / 100) && v.Er < (desiredReturn + 2).toDouble / 100)
+  }
+
+  private def calculateStocksCovariance(indexesReturns: Seq[StockReturnData], monthlyStocksEr: Map[String, Double]) = {
+    for {
+      s1 <- indexesReturns
+      s2 <- indexesReturns
+    } yield {
+      val s1Returns = s1.stockData.map(_.r)
+      val s2Returns = s2.stockData.map(_.r)
+      val s1Er = monthlyStocksEr(s1.stockFileName)
+      val s2Er = monthlyStocksEr(s2.stockFileName)
+
+      val cov = covCalculator.clacCov(s1Returns, s2Returns, s1Er, s2Er)
+      CovData(s1.stockFileName, s2.stockFileName, cov)
+    }
+  }
+
+  private def calculateTheAnnualAverageReturnPerStock(indexesReturns: Seq[StockReturnData]) = {
+    indexesReturns
+      .map(i => i.stockFileName -> PortfolioCalculatorService.calcYearlyAverageReturn(i.stockData.map(_.r)))
+      .toMap
+  }
+
+  private def calculateTheMonthlyAverageReturnPerStock(indexesReturns: Seq[StockReturnData]) = {
+    indexesReturns
+      .map(i =>
+        i.stockFileName -> PortfolioCalculatorService.calcAverageReturn(i.stockData.map(_.r))).toMap
+  }
+
+  private def calculateStockReturns(indexesRawData: Seq[IndexRawData]) = {
+    indexesRawData.map {
+      indexRawData =>
+        val indexReturns: Seq[ReturnData] = returnsCalculator.calculateReturns(indexRawData.stockData)
+        StockReturnData(indexRawData.stockFileName, indexReturns)
+    }
+  }
 }
 
 object PortfolioCalculatorService {
