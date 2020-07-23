fc8d83a8b9dc7fa8de80a2b61e6eb31087804205
barakm <barakm@wix.com>
2020-07-23 10:09:12 +0300

    Created a CSV writer

--- src/main/scala/com/portfolio/app/MyApp.scala
+++ src/main/scala/com/portfolio/app/MyApp.scala
@@ -1,7 +1,7 @@
 package com.portfolio.app
 
 import com.portfolio.cov.DefaultCovCalculator
-import com.portfolio.io.{ CsvDataReader, TxtDataWriter }
+import com.portfolio.io.{ CsvDataReader, CsvDataWriter, TxtDataWriter }
 import com.portfolio.measure.DurationMeasurer
 import com.portfolio.returns.{ DefaultPortfolioReturnCalculator, DefaultReturnsCalculator }
 import com.portfolio.service.PortfolioCalculatorService
@@ -11,7 +11,7 @@ object MyApp extends App {
   val pathToData = "./src/main/resources/market-portfolio"
   val measurer = new DurationMeasurer()
   val dataReader = new CsvDataReader(pathToData)
-  val dataWriter = new TxtDataWriter()
+  val dataWriter = new CsvDataWriter()
   val returnsCalculator = new DefaultReturnsCalculator()
   val covCalculator = new DefaultCovCalculator()
   val varianceCalculator = new DefaultVarianceCalculator(measurer)
--- src/main/scala/com/portfolio/io/CsvDataWriter.scala
+++ src/main/scala/com/portfolio/io/CsvDataWriter.scala
@@ -1,5 +1,36 @@
 package com.portfolio.io
 
-class CsvDataWriter {
+import java.io.File
+
+import com.github.tototoshi.csv.CSVWriter
+import com.portfolio.domain.VectorStdev
+
+class CsvDataWriter extends DataWriter {
+  val dir = "./src/main/resources/results"
+
+  override def writeVectors(fileName: String, vectors: Iterator[VectorStdev]): Unit = {
+    new File(dir).mkdir()
+    val writer: CSVWriter = CSVWriter.open(new File(s"$dir/$fileName"))
+
+    writeRowsToFile(writer, vectors)
+  }
+
+  // TODO: Add AtomicBoolean instead of isFirstIteration
+  @scala.annotation.tailrec
+  private def writeRowsToFile(writer: CSVWriter, vectors: Iterator[VectorStdev], isFirstIteration: Boolean = true): Unit = {
+    if (vectors.hasNext) {
+      val next = vectors.next()
+      if (isFirstIteration) {
+        val headers = next.weights.map(_.stockName) ++ Seq("E(r)", "StDev")
+        val row = next.weights.map(_.weight) ++ Seq(next.Er, next.stdev)
+        writer.writeRow(headers)
+        writer.writeRow(row)
+      } else {
+        val row = next.weights.map(_.weight) ++ Seq(next.Er, next.stdev)
+        writer.writeRow(row)
+      }
+      writeRowsToFile(writer, vectors, isFirstIteration = false)
+    }
+  }
 
 }
