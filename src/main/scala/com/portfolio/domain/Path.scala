8990786809310d8849b6f52a1fe1917bc80afb5f
barakm <barakm@wix.com>
2020-07-31 14:03:11 +0300

    Extract paths to an object file

---
+++ src/main/scala/com/portfolio/domain/Path.scala
@@ -0,0 +1,7 @@
+package com.portfolio.domain
+
+object Path {
+  val yahooFinanceFilesPath = "./src/main/resources/market-portfolio"
+  val productionResultsFilesPath = "./src/main/resources/results"
+  val testResultsFilesPath = "./src/test/resources/test-results"
+}
