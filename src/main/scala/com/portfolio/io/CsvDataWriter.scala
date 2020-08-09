33a88a9aa0017337791ae513c2d3d4ce41e74565
barakm <barakm@wix.com>
2020-08-09 20:39:54 +0300

    Added method to write stocks data to a csv file

--- src/main/scala/com/portfolio/io/CsvDataWriter.scala
+++ src/main/scala/com/portfolio/io/CsvDataWriter.scala
@@ -4,27 +4,39 @@ import java.io.File
 
 import com.github.tototoshi.csv.CSVWriter
 import com.portfolio.domain.VectorStdev
+import com.portfolio.http.Price
 
-class CsvDataWriter(path: String) extends DataWriter {
+class CsvDataWriter(stocksDataPath: String, resultsPath: String) extends DataWriter {
   override def writeVectors(fileName: String, vectors: Iterator[VectorStdev]): Unit = {
-    new File(path).mkdir()
-    val writer: CSVWriter = CSVWriter.open(new File(s"$path/$fileName"))
+    new File(resultsPath).mkdir()
+    val writer: CSVWriter = CSVWriter.open(new File(s"$resultsPath/$fileName.csv"))
 
     writeRowsToFile(writer, vectors)
+  }
+
+  override def writeStockData(fileName: String, stockData: Iterator[Price]): Unit = {
+    new File(stocksDataPath).mkdir()
+    val writer: CSVWriter = CSVWriter.open(new File(s"$stocksDataPath/$fileName.csv"))
+
+    writer.writeRow(Seq("Date", "Adj Close"))
+    stockData.foreach {
+      case Price(date, adjclose) =>
+        writer.writeRow(Seq(date.toString("yyyy-MM-dd"), adjclose))
+    }
   }
 
   // TODO: Add AtomicBoolean instead of isFirstIteration
   @scala.annotation.tailrec
   private def writeRowsToFile(writer: CSVWriter, vectors: Iterator[VectorStdev], isFirstIteration: Boolean = true): Unit = {
     if (vectors.hasNext) {
-      val next = vectors.next()
+      val vector = vectors.next()
       if (isFirstIteration) {
-        val headers = next.weights.map(_.stockName) ++ Seq("E(r)", "StDev")
-        val row = next.weights.map(_.weight) ++ Seq(next.Er, next.stdev)
+        val headers = vector.weights.map(_.stockName) ++ Seq("E(r)", "StDev")
+        val row = vector.weights.map(_.weight) ++ Seq(vector.Er, vector.stdev)
         writer.writeRow(headers)
         writer.writeRow(row)
       } else {
-        val row = next.weights.map(_.weight) ++ Seq(next.Er, next.stdev)
+        val row = vector.weights.map(_.weight) ++ Seq(vector.Er, vector.stdev)
         writer.writeRow(row)
       }
       writeRowsToFile(writer, vectors, isFirstIteration = false)
--- src/main/scala/com/portfolio/io/DataWriter.scala
+++ src/main/scala/com/portfolio/io/DataWriter.scala
@@ -1,7 +1,10 @@
 package com.portfolio.io
 
 import com.portfolio.domain.VectorStdev
+import com.portfolio.http.Price
 
 trait DataWriter {
   def writeVectors(fileName: String, vectors: Iterator[VectorStdev]): Unit
+
+  def writeStockData(fileName: String, stockData: Iterator[Price]): Unit
 }
