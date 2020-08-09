8eea3089c0a9b8e746122f5171a2172c888c101c
barakm <barakm@wix.com>
2020-08-09 20:40:41 +0300

    added data provider to download stocks data from Yahoo finance

--- src/main/scala/com/portfolio/http/DataProvider.scala
+++ src/main/scala/com/portfolio/http/DataProvider.scala
@@ -1,5 +1,23 @@
 package com.portfolio.http
 
-trait DataFetcher {
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.joda.time.DateTime
 
+trait DataProvider {
+  def getHistoricData(symbol: String, frequency: String, from: DateTime, to: DateTime): Response
 }
+
+case class RawResponse(prices: Seq[PriceWithType])
+
+case class PriceWithType(
+                          date: Long,
+                          adjclose: Double,
+                          @JsonProperty("type") `type`: String
+                        )
+
+case class Response(symbol: String, prices: Seq[Price])
+
+case class Price(
+                  date: DateTime,
+                  adjclose: Double,
+                )
--- src/main/scala/com/portfolio/http/YahooFinanceHttpClient.scala
+++ src/main/scala/com/portfolio/http/YahooFinanceHttpClient.scala
@@ -1,14 +1,13 @@
 package com.portfolio.http
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.{ DeserializationFeature, ObjectMapper }
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import org.joda.time.DateTime
 import scalaj.http.Http
 
-class YahooFinanceHttpClient {
-  def getHistoricData(symbol: String, frequency: String, from: DateTime, to: DateTime) = {
+class YahooFinanceHttpClient extends DataProvider {
+  def getHistoricData(symbol: String, frequency: String, from: DateTime, to: DateTime): Response = {
     implicit val mapper = createObjectMapper()
 
     val yahooFinanceResponse = Http("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data")
@@ -27,23 +26,26 @@ class YahooFinanceHttpClient {
     val nonDividendData: PriceWithType => Boolean =
       _.`type` != YahooFinanceHttpClient.dividend
 
+    val nonSplitData: PriceWithType => Boolean =
+      _.`type` != YahooFinanceHttpClient.split
+
     val toPrice: PriceWithType => Price =
       p => Price(new DateTime(p.date * 1000), p.adjclose)
 
     val prices = fromJson[RawResponse](yahooFinanceResponse).prices
       .filter(nonDividendData)
+      .filter(nonSplitData)
       .map(toPrice)
+      .sortBy(_.date)
 
-    Response(prices)
+    Response(symbol, prices)
   }
-
 
   private def createObjectMapper() = {
     val mapper = new ObjectMapper() with ScalaObjectMapper
     mapper.registerModule(DefaultScalaModule)
     mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     mapper
-
   }
 
   private def fromJson[T](json: String)(implicit mapper: ObjectMapper with ScalaObjectMapper, m: Manifest[T]): T =
@@ -56,6 +58,7 @@ class YahooFinanceHttpClient {
 object YahooFinanceHttpClient {
   val get = "GET"
   val dividend = "DIVIDEND"
+  val split = "SPLIT"
   val symbol = "symbol"
   val frequency = "frequency"
   val from = "period1"
@@ -63,26 +66,10 @@ object YahooFinanceHttpClient {
   val filterHistoryData = ("filter", "history")
   val hostHeader = ("x-rapidapi-host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
   val keyHeader = ("x-rapidapi-key", "78ce9280b6mshc900fa7ce09dab8p113ccbjsn286ee4888437")
-
+  val monthly = "1mo"
 }
 
-object F extends App {
-  val y = new YahooFinanceHttpClient
-  y.getHistoricData("SPY", "1mo", DateTime.parse("2015-08-01"), DateTime.parse("2020-08-01"))
-}
-
-case class RawResponse(prices: Seq[PriceWithType])
-
-case class Response(prices: Seq[Price])
-
-case class PriceWithType(
-                          date: Long,
-                          adjclose: Double,
-                          @JsonProperty("type") `type`: String
-                        )
-
-case class Price(
-                  date: DateTime,
-                  adjclose: Double,
-                )
-
+//object F extends App {
+//  val y = new YahooFinanceHttpClient
+//  y.getHistoricData("SPY", "1mo", DateTime.parse("2015-08-01"), DateTime.parse("2020-08-01"))
+//}
