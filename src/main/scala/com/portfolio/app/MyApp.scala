ef3e46202bc29ffb895cf390a0daacc5e48a9d91
barakm <barakm@wix.com>
2020-08-09 20:42:58 +0300

    Added another service for fetching stocks data

--- src/main/scala/com/portfolio/app/MyApp.scala
+++ src/main/scala/com/portfolio/app/MyApp.scala
@@ -3,27 +3,29 @@ package com.portfolio.app
 import com.portfolio.analysis.PortfoliosAnalyzer
 import com.portfolio.cov.DefaultCovCalculator
 import com.portfolio.domain.Path
+import com.portfolio.http.YahooFinanceHttpClient
 import com.portfolio.io.{ CsvDataWriter, DefaultPortfoliosReader, YahooFinanceCsvDataReader }
 import com.portfolio.measure.DurationMeasurer
 import com.portfolio.returns.{ DefaultPortfolioReturnCalculator, DefaultReturnsCalculator }
-import com.portfolio.service.PortfolioCalculatorService
+import com.portfolio.service.{ FetchDataService, PortfolioCalculatorService }
 import com.portfolio.variance.DefaultVarianceCalculator
 
 object MyApp extends App {
-  val allPortfoliosFileName = "all-portfolios.csv"
-  val efficientFrontierFileName = "efficient-frontier.csv"
+  val allPortfoliosFileName = "all-portfolios"
+  val efficientFrontierFileName = "efficient-frontier"
 
   val measurer = new DurationMeasurer()
   val dataReader = new YahooFinanceCsvDataReader(Path.yahooFinanceDirPath)
-  val dataWriter = new CsvDataWriter(Path.productionResultsDirPath)
+  val dataWriter = new CsvDataWriter(Path.yahooFinanceDirPath, Path.productionResultsDirPath)
   val returnsCalculator = new DefaultReturnsCalculator()
   val covCalculator = new DefaultCovCalculator()
   val varianceCalculator = new DefaultVarianceCalculator(measurer)
   val portfolioReturnCalculator = new DefaultPortfolioReturnCalculator()
   val portfoliosReader = new DefaultPortfoliosReader(Path.productionResultsDirPath)
   val portfoliosAnalyzer = new PortfoliosAnalyzer(portfoliosReader, dataWriter)
+  val dataProvider = new YahooFinanceHttpClient
 
-  val service = new PortfolioCalculatorService(
+  val portfolioCalculatorService = new PortfolioCalculatorService(
     dataReader,
     dataWriter,
     returnsCalculator,
@@ -34,6 +36,9 @@ object MyApp extends App {
     measurer = measurer,
     portfoliosAnalyzer)
 
-  //  measurer.measure("calculateMarketPortfolio", service.calculateAllPortfolios(allPortfoliosFileName))
-  measurer.measure("calculateEfficientFrontier", service.calculateEfficientFrontier(efficientFrontierFileName))
+  val fetchDataService = new FetchDataService(dataProvider, dataWriter)
+
+  //    measurer.measure("calculateMarketPortfolio", portfolioCalculatorService.calculateAllPortfolios(allPortfoliosFileName))
+  measurer.measure("calculateEfficientFrontier", portfolioCalculatorService.calculateEfficientFrontier(efficientFrontierFileName))
+//    fetchDataService.downloadStockData()
 }
--- src/main/scala/com/portfolio/service/FetchDataService.scala
+++ src/main/scala/com/portfolio/service/FetchDataService.scala
@@ -1,5 +1,39 @@
 package com.portfolio.service
 
-class FetchDataService {
+import com.portfolio.http.{ DataProvider, Response, YahooFinanceHttpClient }
+import com.portfolio.io.DataWriter
+import org.joda.time.DateTime
 
+class FetchDataService(dataProvider: DataProvider, dataWriter: DataWriter) {
+
+  def downloadStockData() =
+    FetchDataService.listOfSymbols.map {
+      symbol =>
+        dataProvider.getHistoricData(
+          symbol,
+          YahooFinanceHttpClient.monthly,
+          FetchDataService.from,
+          FetchDataService.to
+        )
+    } foreach {
+      case Response(symbol, prices) =>
+        dataWriter.writeStockData(symbol, prices.iterator)
+    }
+
+}
+
+object FetchDataService {
+  val to = DateTime.now().minusDays(2)
+  val from = to.minusYears(15)
+
+  val listOfSymbols =
+    "SPY" ::
+      "QQQ" ::
+      "BGNE" ::
+      "MSCI" ::
+      "TA35.TA" ::
+      "VYM" ::
+      "XBI" ::
+      "XLE" ::
+      "DAX" :: Nil
 }
